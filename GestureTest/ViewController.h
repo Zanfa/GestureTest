1f725f7bc274580b43c4b8ed3886b3dbe759a53d
Vahur Roosimaa <vahur@scopely.com>
2013-05-20 20:27:23 +0300

    Initial Commit

---
+++ GestureTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GestureTest
+//
+//  Created by Vahur Roosimaa on 5/20/13.
+//  Copyright (c) 2013 Vahur Roosimaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GestureTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GestureTest
+//
+//  Created by Vahur Roosimaa on 5/20/13.
+//  Copyright (c) 2013 Vahur Roosimaa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
